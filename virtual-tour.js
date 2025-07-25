9d15e4836a47943ef3a15eaa9f6a1008eaa6df59
RobertCs84 <90334106+RobertCs84@users.noreply.github.com>
2025-07-25 14:30:34 +0200

    Add files via upload

--- virtual-tour.js
+++ virtual-tour.js
@@ -8,9 +8,8 @@ const checkpoints = [
   { id: 3, name: "Huszárokelőpuszta", lat: 47.3146701, lon: 17.6887561 },
   { id: 4, name: "Veszprém", lat: 47.1189646, lon: 17.9114044 },
   { id: 5, name: "Meggyespuszta", lat: 47.0517992, lon: 17.9374564 },
-  { id: 6, name: "Alsóőrs", lat: 46.9854293, lon: 17.9743677 },
-  { id: 7, name: "Csóka utca", lat: 47,4706375, lon: 19,0277570 }
-];
+  { id: 6, name: "Alsóőrs", lat: 46.9854293, lon: 17.9743677 }
+ ];
 
 window.onload = function () {
   const logged = localStorage.getItem('loggedInUser');
